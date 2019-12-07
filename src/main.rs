f217ff2ede59a5407aee6ae5c560b9770493e819
Robert Hodgkiss <r.hodgkiss@uea.ac.uk>
2019-12-07 22:43:57 +0000

    Completed puzzle 021

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ mod puzzle01;
 mod puzzle02;
 
 fn main() {
-    puzzle02::puzzle021("./content/input021.txt");
+    print!("{}\n", puzzle02::puzzle021("./content/input021.txt"));
 }
--- src/puzzle02.rs
+++ src/puzzle02.rs
@@ -2,29 +2,46 @@ use std::fs::File;
 use std::io::prelude::*;
 use regex::Regex;
 
-fn openFile(path: &str) -> String{
+fn open_file(path: &str) -> String{
     let mut input = File::open(path).expect("Failed to open input file");
     let mut input_buffer = String::new();
     input.read_to_string(&mut input_buffer).expect("Failed to read from input file");
     return input_buffer;
 }
 
-fn execute(data: Vec<u64>) -> Vec<u64>{
+fn display(data: Vec<u64>){
     for i in data.iter(){
-        print!("{}", i);
+        print!("{}\n", i);
     }
-    let v: Vec<u64> = Vec::new();
-    return v;
+}
+
+fn execute(mut data: Vec<u64>) -> Vec<u64>{
+    let mut i: u16 = 0;
+    while i < data.len() as u16{
+        if data[i as usize] == 1{
+            let x: u64 = data[(i + 1) as usize];
+            let y: u64 = data[(i + 2) as usize];
+            let z: u64 = data[(i + 3) as usize];
+            print!("{}({}) = {} + {}\n", z, data[z as usize], data[x as usize], data[y as usize]);
+            data[z as usize] = data[x as usize] + data[y as usize];
+        }else if data[i as usize] == 2{
+            let x: u64 = data[(i + 1) as usize];
+            let y: u64 = data[(i + 2) as usize];
+            let z: u64 = data[(i + 3) as usize];
+            print!("{}({}) = {} * {}\n", z, data[z as usize], data[x as usize], data[y as usize]);
+            data[z as usize] = data[x as usize] * data[y as usize];
+        }
+        i = i + 4;
+    }
+    return data;
 }
 
 pub fn puzzle021(path: &str) -> u64{
-    let data: &str = &openFile(path)[..];
+    let data: &str = &open_file(path)[..];
     let mut vec: Vec<u64> = Vec::new();
     for i in Regex::new(r",").unwrap().split(data){
         vec.push(i.parse::<u64>().expect("Failed to parse string"));
     }
-    for i in vec.iter(){
-        print!("Contains {}\n", i);
-    }
-    return 0;
+    vec = execute(vec);
+    return vec[0 as usize];
 }
