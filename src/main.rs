fba65d66eaf1525de090b4534a9bd92b6951a55a
SuedeGently <robertdhodgkiss@gmail.com>
2019-12-16 14:51:50 +0000

    Completed puzzle 031 + Completed puzzle can be ran using fn puzzle031

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod puzzle03;
 
 /// Handles command-line arguments
 fn main() {
-    let mut grid: puzzle03::Grid = puzzle03::Grid::grid();
-    grid.execute("./content/input031.txt");
+    let mut grid1: puzzle03::Grid = puzzle03::Grid::grid();
+    let mut grid2: puzzle03::Grid = puzzle03::Grid::grid();
+
+    puzzle03::Grid::puzzle1(&mut grid1, &mut grid2, "./content/input031.txt");
 }
--- src/puzzle03.rs
+++ src/puzzle03.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use regex::Regex;
+use std::collections::HashSet;
 
 pub enum Movement{
     RIGHT(i64),
@@ -10,7 +11,7 @@ pub enum Movement{
 }
 
 pub struct Grid{
-    wires: Vec<i64>,
+    wires: HashSet<Vec<i64>>,
     current_pos: Vec<i64>
 }
 
@@ -40,7 +41,7 @@ pub fn largestValue(path: &str) -> i64{
 impl Grid{
     pub fn grid() -> Grid{
         let grid: Grid = Grid{
-            wires: vec![0;0],
+            wires: HashSet::new(),
             current_pos: vec![0; 2]
         };
         return grid;
@@ -53,18 +54,66 @@ impl Grid{
     fn move_wire(&mut self, movement: Movement){
         match movement{
             Movement::RIGHT(distance) =>
-                for i in self.current_pos[0]..(self.current_pos[0] + distance){
-                    // push to array
+                for i in self.current_pos[0]..(self.current_pos[0] + distance + 1){
+                    self.current_pos[0] = i;
+                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    }
                 },
-            Movement::LEFT(distance) => println!("Left: {}\n", distance),
-            Movement::UP(distance) => println!("Up: {}\n", distance),
-            Movement::DOWN(distance) => println!("Down: {}\n", distance)
+            Movement::LEFT(distance) =>
+                for i in ((self.current_pos[0] - distance)..self.current_pos[0] + 1).rev(){
+                    self.current_pos[0] = i;
+                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    }
+                },
+            Movement::UP(distance) =>
+                for i in self.current_pos[1]..(self.current_pos[1] + distance + 1){
+                    self.current_pos[1] = i;
+                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    }
+                },
+            Movement::DOWN(distance) =>
+                for i in ((self.current_pos[1] - distance)..self.current_pos[1] + 1).rev(){
+                    self.current_pos[1] = i;
+                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    }
+                }
         }
     }
 
-    pub fn execute(&mut self, path: &str){
+    fn execute(&mut self, instructions: &str){
+        for j in Regex::new(r"\w\d+").unwrap().captures_iter(&instructions){
+            for k in j[0].lines(){
+                match &k[..1]{
+                    "R" => self.move_wire(Movement::RIGHT(k[1..].parse::<i64>().unwrap())),
+                    "L" => self.move_wire(Movement::LEFT(k[1..].parse::<i64>().unwrap())),
+                    "U" => self.move_wire(Movement::UP(k[1..].parse::<i64>().unwrap())),
+                    "D" => self.move_wire(Movement::DOWN(k[1..].parse::<i64>().unwrap())),
+                    _ => println!("Invalid character")
+                }
+            }
+        }
+    }
+
+    fn compare(grid1: &Grid, grid2: &Grid) -> i64{
+        let mut best: i64 = 0;
+        for i in &grid1.wires{
+            if grid2.wires.contains(i){
+                if best == 0 || ((i[0].abs() + i[1].abs()) < best && (i[0].abs() + i[1].abs()) != 0){
+                    best = i[0].abs() + i[1].abs();
+                }
+                println!("Checked {}, {}", i[0], i[1]);
+            }
+        }
+        return best;
+    }
+
+    pub fn puzzle1(grid1: &mut Grid, grid2: &mut Grid, path: &str){
         let mut input = File::open(path)
-          .expect("Grid::execute() failed to open input file");
+          .expect("Grid::puzzle1() failed to open input file");
         let mut input_buffer = String::new();
         let mut largest: i64 = 0;
 
@@ -72,18 +121,22 @@ impl Grid{
           .read_to_string(&mut input_buffer)
           .expect("Failed to read from input file");
         
-        for i in input_buffer.lines(){
-            for j in Regex::new(r"\w\d+").unwrap().captures_iter(&input_buffer[..]){
-                for k in j[0].lines(){
-                    match &k[..1]{
-                        "R" => self.move_wire(Movement::RIGHT(k[1..].parse::<i64>().unwrap())),
-                        "L" => self.move_wire(Movement::LEFT(k[1..].parse::<i64>().unwrap())),
-                        "U" => self.move_wire(Movement::UP(k[1..].parse::<i64>().unwrap())),
-                        "D" => self.move_wire(Movement::RIGHT(k[1..].parse::<i64>().unwrap())),
-                        _ => println!("Invalid character")
-                    }
-                }
-            }
+        let mut wire_instructions: std::str::Lines = input_buffer.lines();
+        let mut wire1: &str = wire_instructions.next().unwrap();
+        let mut wire2: &str = wire_instructions.next().unwrap();
+
+        println!("Building first wire...");
+        grid1.execute(wire1);
+        println!("Building second wire...");
+        grid2.execute(wire2);
+
+        println!("Finding best match...");
+        println!("Best match is {}", Grid::compare(grid1, grid2));
+    }
+
+    pub fn display(&self){
+        for i in &self.wires{
+            println!("{}, {}", i[0], i[1]);
         }
     }
 }
