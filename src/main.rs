5eac25242f02d91b6cb522dbbe6d615b791f6c04
SuedeGently <robertdhodgkiss@gmail.com>
2019-12-16 15:28:21 +0000

    Added puzzle031 handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,50 @@
+use clap::*;
+
+mod puzzle01;
+mod puzzle02;
 mod puzzle03;
 
 /// Handles command-line arguments
 fn main() {
-    let mut grid1: puzzle03::Grid = puzzle03::Grid::grid();
-    let mut grid2: puzzle03::Grid = puzzle03::Grid::grid();
-
-    puzzle03::Grid::puzzle1(&mut grid1, &mut grid2, "./content/input031.txt");
+    let matches = App::new("aoc-2019")
+                          .version("0.2")
+                          .author("SuedeGently")
+                          .about("Advent of Code 2019")
+                          .arg(Arg::with_name("PUZZLE")
+                               .long("puzzle")
+                               .value_name("puzzle")
+                               .help("sets the desired puzzle")
+                               .index(1)
+                               .required(true)
+                               .takes_value(true))
+                          .arg(Arg::with_name("FILE")
+                                .long("path")
+                                .short("p")
+                                .value_name("Path to file")
+                                .help("Sets the input file to use"))
+                          .get_matches();
+    
+    if matches.value_of("PUZZLE").unwrap() == "01"{
+        let path: &str = matches
+          .value_of("FILE")
+          .unwrap_or("./content/input011.txt");
+        print!("Fuel needed: {}\n\n", puzzle01::calculate_fuel(path));
+    }else if matches.value_of("PUZZLE").unwrap() == "021"{
+        let path: &str = matches
+          .value_of("FILE")
+          .unwrap_or("./content/input021.txt");
+        print!("{}\n", puzzle02::puzzle021(path));
+    }else if matches.value_of("PUZZLE").unwrap() == "022"{
+        let path: &str = matches
+          .value_of("FILE")
+          .unwrap_or("./content/input021.txt");
+        print!("{}\n", puzzle02::puzzle022(path, 19690720));
+    }else if matches.value_of("PUZZLE").unwrap() == "031"{
+        let path: &str = matches
+          .value_of("FILE")
+          .unwrap_or("./content/input031.txt");
+        print!("{}\n", puzzle02::puzzle022(path, 19690720));
+    }else{
+        print!("Didn't work :(\n");
+    }
 }
