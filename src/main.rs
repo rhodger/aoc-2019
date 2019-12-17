2fb99cbdc6b80cb8d7a6a56676926bd668ec8def
SuedeGently <robertdhodgkiss@gmail.com>
2019-12-17 11:44:08 +0000

    Completed puzzle 032

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,5 @@ fn main() {
     let mut grid1: puzzle03::Grid = puzzle03::Grid::grid();
     let mut grid2: puzzle03::Grid = puzzle03::Grid::grid();
 
-    puzzle03::Grid::puzzle1(&mut grid1, &mut grid2, "./content/input031.txt");
+    puzzle03::Grid::puzzle2(&mut grid1, &mut grid2, "./content/input031.txt");
 }
--- src/puzzle03.rs
+++ src/puzzle03.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 use regex::Regex;
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 pub enum Movement{
     RIGHT(i64),
@@ -11,7 +11,7 @@ pub enum Movement{
 }
 
 pub struct Grid{
-    wires: HashSet<Vec<i64>>,
+    wires: HashMap<Vec<i64>, i64>,
     current_pos: Vec<i64>
 }
 
@@ -41,7 +41,7 @@ pub fn largestValue(path: &str) -> i64{
 impl Grid{
     pub fn grid() -> Grid{
         let grid: Grid = Grid{
-            wires: HashSet::new(),
+            wires: HashMap::new(),
             current_pos: vec![0; 2]
         };
         return grid;
@@ -51,68 +51,91 @@ impl Grid{
         return vec![self.current_pos[0], self.current_pos[1]];
     }
 
-    fn move_wire(&mut self, movement: Movement){
+    fn move_wire(&mut self, movement: Movement, current_steps: i64) -> i64{
+        let mut new_steps: i64 = current_steps;
         match movement{
             Movement::RIGHT(distance) =>
                 for i in self.current_pos[0]..(self.current_pos[0] + distance + 1){
                     self.current_pos[0] = i;
-                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
-                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    if !self.wires.contains_key(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]], new_steps);
+                    }else{
+                        *self.wires.get_mut(&vec![self.current_pos[0], self.current_pos[1]]).unwrap() += 1;
                     }
+                    new_steps += 1;
                 },
             Movement::LEFT(distance) =>
                 for i in ((self.current_pos[0] - distance)..self.current_pos[0] + 1).rev(){
                     self.current_pos[0] = i;
-                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
-                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    if !self.wires.contains_key(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]], new_steps);
+                    }else{
+                        *self.wires.get_mut(&vec![self.current_pos[0], self.current_pos[1]]).unwrap() += 1;
                     }
+                    new_steps += 1;
                 },
             Movement::UP(distance) =>
                 for i in self.current_pos[1]..(self.current_pos[1] + distance + 1){
-                    self.current_pos[1] = i;
-                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
-                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    self.current_pos[1] = i;                    
+                    if !self.wires.contains_key(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]], new_steps);
+                    }else{
+                        *self.wires.get_mut(&vec![self.current_pos[0], self.current_pos[1]]).unwrap() += 1;
                     }
+                    new_steps += 1;
                 },
             Movement::DOWN(distance) =>
                 for i in ((self.current_pos[1] - distance)..self.current_pos[1] + 1).rev(){
                     self.current_pos[1] = i;
-                    if !self.wires.contains(&vec![self.current_pos[0], self.current_pos[1]]){
-                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]]);
+                    if !self.wires.contains_key(&vec![self.current_pos[0], self.current_pos[1]]){
+                        self.wires.insert(vec![self.current_pos[0], self.current_pos[1]], new_steps);
+                    }else{
+                        *self.wires.get_mut(&vec![self.current_pos[0], self.current_pos[1]]).unwrap() += 1;
                     }
+                    new_steps += 1;
                 }
         }
+        return new_steps;
     }
 
     fn execute(&mut self, instructions: &str){
+        let mut steps: i64 = 0;
         for j in Regex::new(r"\w\d+").unwrap().captures_iter(&instructions){
             for k in j[0].lines(){
                 match &k[..1]{
-                    "R" => self.move_wire(Movement::RIGHT(k[1..].parse::<i64>().unwrap())),
-                    "L" => self.move_wire(Movement::LEFT(k[1..].parse::<i64>().unwrap())),
-                    "U" => self.move_wire(Movement::UP(k[1..].parse::<i64>().unwrap())),
-                    "D" => self.move_wire(Movement::DOWN(k[1..].parse::<i64>().unwrap())),
+                    "R" => steps = self.move_wire(Movement::RIGHT(k[1..].parse::<i64>().unwrap()), steps) - 1,
+                    "L" => steps = self.move_wire(Movement::LEFT(k[1..].parse::<i64>().unwrap()), steps) - 1,
+                    "U" => steps = self.move_wire(Movement::UP(k[1..].parse::<i64>().unwrap()), steps) - 1,
+                    "D" => steps = self.move_wire(Movement::DOWN(k[1..].parse::<i64>().unwrap()), steps) - 1,
                     _ => println!("Invalid character")
                 }
+                println!("Took {} steps", steps);
             }
-        }
-    }
-
-    fn execute_test(&mut self, instructions1: &str, instructions2: &str){
-        let j = Regex::new(r"\w\d+").unwrap().captures_iter(&instructions);
-        for j in Regex::new(r"\w\d+").unwrap().captures_iter(&instructions){
-            println!("{:?}", j[0].lines().next());
         }
     }
 
     fn compare(grid1: &Grid, grid2: &Grid) -> i64{
         let mut best: i64 = 0;
-        for i in &grid1.wires{
-            if grid2.wires.contains(i){
+        for (i, j) in &grid1.wires{
+            if grid2.wires.contains_key(i){
                 if best == 0 || ((i[0].abs() + i[1].abs()) < best && (i[0].abs() + i[1].abs()) != 0){
                     best = i[0].abs() + i[1].abs();
                 }
                 println!("Checked {}, {}", i[0], i[1]);
+            }
+        }
+        return best;
+    }
+
+    fn compare_steps(grid1: &Grid, grid2: &Grid) -> i64{
+        let mut best: i64 = 0;
+        for (i, j) in &grid1.wires{
+            if grid2.wires.contains_key(i){
+                println!("Intersection @ {}, {}", i[0], i[1]);
+                if (i[0] != 0 || i[1] != 0) && ((j + grid2.wires.get(i).unwrap() < best) || best == 0){
+                    println!("New best: {} & {}", j, grid2.wires.get(i).unwrap());
+                    best = (j + grid2.wires.get(i).unwrap());
+                }
             }
         }
         return best;
@@ -133,9 +156,9 @@ impl Grid{
         let mut wire2: &str = wire_instructions.next().unwrap();
 
         println!("Building first wire...");
-        grid1.execute_test(wire1);
+        grid1.execute(wire1);
         println!("Building second wire...");
-        grid2.execute_test(wire2);
+        grid2.execute(wire2);
 
         println!("Finding best match...");
         println!("Best match is {}", Grid::compare(grid1, grid2));
@@ -143,7 +166,30 @@ impl Grid{
 
     pub fn display(&self){
         for i in &self.wires{
-            println!("{}, {}", i[0], i[1]);
+            println!("{}, {} : {}", i.0[0], i.0[1], i.1);
         }
     }
-}
+
+
+    pub fn puzzle2(grid1: &mut Grid, grid2: &mut Grid, path: &str){
+        let mut input = File::open(path)
+          .expect("Grid::puzzle1() failed to open input file");
+        let mut input_buffer = String::new();
+        let mut largest: i64 = 0;
+
+        input
+          .read_to_string(&mut input_buffer)
+          .expect("Failed to read from input file");
+        
+        let mut wire_instructions: std::str::Lines = input_buffer.lines();
+        let mut wire1: &str = wire_instructions.next().unwrap();
+        let mut wire2: &str = wire_instructions.next().unwrap();
+
+        println!("Building first wire...");
+        grid1.execute(wire1);
+        println!("Building second wire...");
+        grid2.execute(wire2);
+
+        println!("Finding best match...");
+        println!("Best match is {}", Grid::compare_steps(grid1, grid2));
+    }}
